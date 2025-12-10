bb0ae8585b1fe4346fc770dfd09fbca62fecb6a1
ESunRoc <elisun@icloud.com>
2025-12-10 11:38:44 -0500

    Added functions, data, and documentation. Fixed global "assign" bug.

---
+++ R/hdmvmed-package.R
@@ -0,0 +1,11 @@
+#' @importFrom glmnet glmnet cv.glmnet
+#' @importFrom broom tidy
+#' @importFrom MSGLasso FindingPQGrps Cal_grpWTs FindingGRGrps MSGLasso.cv MSGLasso
+#' @importFrom utils capture.output
+#' @importFrom coxed bca
+#' @importFrom DT datatable
+#' @importFrom foreach `%dopar%` foreach
+#' @importFrom doParallel registerDoParallel
+#' @importFrom parallel detectCores makeCluster stopCluster
+#' @importFrom stats lm quantile sd
+NULL
